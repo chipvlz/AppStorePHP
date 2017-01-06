67c6b46648055196ec529e9485a771ef7a6b0b1d
pial08 <pialcsedu19@gmail.com>
2017-01-06 22:36:43 +0600

    2 files added

---
+++ home.php
@@ -0,0 +1,285 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Welcome</title>
+        <link href="css/homepage_style.css" rel="stylesheet" type="text/css"  media="all" />
+        <meta name="keywords" content="Mobilestore iphone web template, Andriod web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+        <link href='http://fonts.googleapis.com/css?family=Londrina+Solid|Coda+Caption:800|Open+Sans' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" href="css/homepage_responsiveslides.css">
+        <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="js/responsiveslides.min.js"></script>
+          <script>
+            // You can also use "$(window).load(function() {"
+                $(function () {
+
+                  // Slideshow 1
+                  $("#slider1").responsiveSlides({
+                    maxwidth: 1600,
+                    speed: 600
+                  });
+            });
+          </script>
+    </head>
+
+    <body>
+        <div class="wrap">
+        <!----start-Header---->
+        <div class="header">
+            <div class="search-bar">
+                <form>
+                    <input type="text"><input type="submit" value="Search" />
+                </form>
+            </div>
+            <div class="clear"> </div>
+        </div>
+        </div>
+
+        <div class="clear"> </div>
+
+        <div class="top-header">
+            <div class="wrap">
+        <!----start-logo---->
+            <div class="logo">
+                <a href="home.php">
+                    <img src="images/Logo.png" title="logo"/>
+                </a>
+            </div>
+        <!----end-logo---->
+        <!----start-top-nav---->
+
+        <div class="top-nav">
+            <ul>
+                <li><a href="index.php" id="loginform">Login</a><li>
+                <li style="padding: 10px 1px;">|</li>
+                <li><a href="register.php">Register</a></li>
+                <li><a href="newHome/index.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="clear"> </div>
+        </div>
+        </div>
+        <!----End-top-nav---->
+        <!----End-Header---->
+    <!--start-image-slider---->
+    <div class="content-grids">
+      <div class="wrap">
+        <br><br><br>
+      <h4>WELCOME TO OUR APP STORE!!!</h4>
+      <p style="font-family: 'Open Sans', sans-serif;font-size: 1.2em;color:#399649;" >
+        A very warm welcome to you, our most precious guest. The first step has already taken by you
+        to get an access to the one of the most enjoying and entertaining app stores of these days :
+        "OS INDEPENDENT APP STORE". Here we can guarantee you for very much good rated and reviewed apps from our users all over the world.<br>
+        Special feature of our site is that we contain apps of all categories of operating systems till date. We also look forward to enhance our database in case of any new OS availability.
+        <br>
+        Our site contains apps of all categories that you can imagine for every purpose you need. More importantly, our next generation children are bound to take many learning aps from the site if you just tour around our website for some time. We are glad to have you for now. Please roam around and please be our honorable member to get access to our huge database of apps.<br>
+        Our honorable members, please proceed as usual.<br>
+        Thank you.<br>
+        <br><br>
+      </p>
+      </div>
+    </div>
+    <div class="content-grids2">
+        <div class="wrap">
+            <div class="image-slider">
+            <!-- Slideshow 1 -->
+                <ul class="rslides" id="slider1">
+                  <li><img src="images/app4.png" alt="app4"></li>
+                  <li><img src="images/app10.png" alt="app10"></li>
+                  <li><img src="images/app12.png" alt="app12"></li>
+                </ul>
+            <!-- Slideshow 2 -->
+            </div>
+    <!--End-image-slider---->
+        </div></div>
+        <div class="content-sidebar2">
+                <h4>Operating Systems</h4>
+                <ul>
+                    <li><a href="windows_os.html">Windows</a></li>
+                    <li><a href="android_os.html">Android</a></li>
+                    <li><a href="iOS_os.html">iOS</a></li>
+                    <li><a href="linux_os.html">LINUX</a></li>
+                </ul>
+            </div>
+
+            <div class="clear"> </div>
+            <div class="wrap">
+            <div class="content">
+                <div class="top-3-grids">
+                    <div class="section group">
+                <div class="grid_1_of_3 images_1_of_3">
+                      <a href="single_guest.php"><img src="images/app3.jpg"></a>
+                      <h3>Minny's Football</h3>
+                </div>
+                <div class="grid_1_of_3 images_1_of_3 second">
+                       <a href="single_guest.php"><img src="images/app11.jpg"></a>
+                      <h3>Temple Run Exclusive</h3>
+                </div>
+                <div class="grid_1_of_3 images_1_of_3 theree">
+                       <a href="single_guest.php"><img src="images/app8.jpg"></a>
+                      <h3>Want to mine?</h3>
+                </div>
+            </div>
+                </div>
+
+            <div class="content-grids">
+                <h4>Top apps of the day</h4>
+                <div class="section group">
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/app2.png">
+                     <a href="single_guest.php">Bad Ball</a>
+                     <h3>$260</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.php"> </a></li>
+                        <li><a  class="Compar" href="single_guest.php"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.php"> </a></li>
+                     </ul>
+                </div>
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/app5.png">
+                      <a href="single_guest.php">Garden Mania</a>
+                     <h3>$150</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.php"> </a></li>
+                        <li><a  class="Compar" href="single_guest.html"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.html"> </a></li>
+                     </ul>
+                </div>
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/app7.png">
+                      <a href="single_guest.php">Quizzzz...</a>
+                     <h3>$130</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.html"> </a></li>
+                        <li><a  class="Compar" href="single_guest.html"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.html"> </a></li>
+                     </ul>
+                </div>
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/Boom.png">
+                      <a href="single_guest.php">Boom Beach</a>
+                     <h3>$460</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.html"> </a></li>
+                        <li><a  class="Compar" href="single_guest.html"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.html"> </a></li>
+                     </ul>
+                </div>
+            </div>
+            <div class="section group">
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/royale.png">
+                     <a href="single_guest.php">Clash Royale</a>
+                     <h3>$260</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.html"> </a></li>
+                        <li><a  class="Compar" href="single_guest.html"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.html"> </a></li>
+                     </ul>
+                </div>
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/BSquad.png">
+                      <a href="single_guest.php">Bomb Squad</a>
+                     <h3>$100</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.html"> </a></li>
+                        <li><a  class="Compar" href="single_guest.html"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.html"> </a></li>
+                     </ul>
+                </div>
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/img4.png">
+                      <a href="single_guest.php">Angry Horse</a>
+                     <h3>$180</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.html"> </a></li>
+                        <li><a  class="Compar" href="single_guest.html"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.html"> </a></li>
+                     </ul>
+                </div>
+                <div class="grid_1_of_4 images_1_of_4 products-info">
+                     <img src="images/app9.png">
+                      <a href="single_guest.php">Cut the Rope 2</a>
+                     <h3>$140</h3>
+                     <ul>
+                        <li><a  class="i" href="single_guest.html"> </a></li>
+                        <li><a  class="Compar" href="single_guest.html"> </a></li>
+                        <li><a  class="Wishlist" href="single_guest.html"> </a></li>
+                     </ul>
+                </div>
+            </div>
+
+                </div>
+                <div class="content-sidebar">
+                    <h4>Categories</h4>
+                        <ul>
+                            <li><a href="#">Books</a></li>
+                            <li><a href="#">Business</a></li>
+                            <li><a href="#">Communication</a></li>
+                            <li><a href="#">Education</a></li>
+                            <li><a href="#">Entertainment</a></li>
+                            <li><a href="#">Games</a></li>
+                            <li><a href="#">Health</a></li>
+                            <li><a href="#">Lifestyle</a></li>
+                            <li><a href="#">Media</a></li>
+                            <li><a href="#">Mobile Phones</a></li>
+                            <li><a href="#">Music</a></li>
+                            <li><a href="#">Photography</a></li>
+                            <li><a href="#">Shopping</a></li>
+                            <li><a href="#">Sports</a></li>
+                            <li><a href="#">Transformation</a></li>
+                            <li><a href="#">Weather</a></li>
+                        </ul>
+                </div>
+            </div>
+            <div class="clear"> </div>
+            </div>
+
+        <div class="footer">
+            <div class="wrap">
+            <div class="section group">
+                <div class="col_1_of_4 span_1_of_4">
+                    <h3>Credits</h3>
+                    <p>Feel free to contact us
+                        <a href="contact.html">here.</a>
+                    </p>
+                </div>
+                <div class="col_1_of_4 span_1_of_4">
+                    <h3>Latest News:</h3>
+                    <p>New Android App has been uploaded daily now.</p>
+                    <p>iOS updates are more advanced.</p>
+                    <p>LINUX apps getting more accepted by people.</p>
+                </div>
+                <div class="col_1_of_4 span_1_of_4">
+                    <h3>Store Location</h3>
+                    <p>Yet to be announced. :p</p>
+                    <h3>Order-online</h3>
+                    <p>01685-604351</p>
+                    <p>123-345-678</p>
+                </div>
+                <div class="col_1_of_4 span_1_of_4 footer-lastgrid">
+                    <h3>News-Letter</h3>
+                    <input type="text"><input type="submit" value="go" />
+                    <h3>Follow Us:</h3>
+                     <ul>
+                        <li><a href="https://twitter.com/?lang=en"><img src="images/twitter.png" title="twitter" />Twitter</a></li>
+                        <li><a href="https://www.facebook.com/"><img src="images/facebook.png" title="Facebook" />Facebook</a></li>
+                        <li><a href="https://www.rss.com/"><img src="images/rss.png" title="Rss" />Rss</a></li>
+                     </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="clear"> </div>
+        <div class="wrap">
+        <div class="copy-right">
+            <p>Mobilestore  &#169    All Rights Reserved | Design By <a href="contact.html">Roll08&12</a></p>
+        </div>
+        </div>
+        </div>
+    </body>
+</html>
+
---
+++ index.php
@@ -0,0 +1,152 @@
+<?php
+	
+	session_start();
+	include_once "dbhelper.php";
+
+	if(isset($_SESSION['user'])!="")
+	{
+		//echo "damn!!";
+		header("Location: newHome/index.php");	// change probably done
+	}
+
+?>
+
+
+<!DOCTYPE html>
+<html >
+  <head>
+  <title>Log In</title>
+  <link href="css/homepage_style.css" rel="stylesheet" type="text/css"  media="all" />
+  <meta name="keywords" content="Mobilestore iphone web template, Andriod web template, Smartphone web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+  <link href='http://fonts.googleapis.com/css?family=Londrina+Solid|Coda+Caption:800|Open+Sans' rel='stylesheet' type='text/css'>
+  <link rel="stylesheet" href="css/homepage_responsiveslides.css">
+  <link rel="stylesheet" type="text/css" href="css/bootstrap.css">
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+        <script src="js/responsiveslides.min.js"></script>
+          <script>
+            // You can also use "$(window).load(function() {"
+                $(function () {
+
+                  // Slideshow 1
+                  $("#slider1").responsiveSlides({
+                    maxwidth: 1600,
+                    speed: 600
+                  });
+            });
+          </script>
+  <script src="js/prefixfree.min.js"></script>
+  </head>
+
+  <body>
+      <div class="wrap">
+        <!----start-Header---->
+        <div class="header">
+            <div class="search-bar">
+                <form>
+                    <input type="text"><input type="submit" value="Search" />
+                </form>
+            </div>
+            <div class="clear"> </div>
+        </div>
+        </div>
+
+        <div class="clear"> </div>
+
+        <div class="top-header">
+            <div class="wrap">
+        <!----start-logo---->
+            <div class="logo">
+                <a href="newHome/index.php">
+                    <img src="images/Logo.png" title="logo"/>
+                </a>
+            </div>
+        <!----end-logo---->
+        <!----start-top-nav---->
+
+        <div class="top-nav">
+            <ul>
+                
+                <li><a href="register.php">Register</a></li>
+                <li><a href="newHome/index.php">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Store</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+        </div>
+        <div class="clear"> </div>
+        </div>
+        </div>
+        <!----End-top-nav---->
+        <!----End-Header---->
+    <!--start-image-slider---->
+
+
+        <form action="loginaction.php" method="post">
+          <br><br>
+          <fieldset class="text-center" style="padding: 10px 20px; width:50%;margin:auto;">
+            <legend style="font-size:1.2em; color:#94CB32;">Log In here</legend>
+            <label for="" style="font-family: 'Open Sans', sans-serif;color: #399649;">Log in as:<br><br></label>
+              <input type="radio" name="person" value="admin" checked style="color:#94CB32;">Admin<br>
+              <input type="radio" name="person" value="user" style="color:#94CB32;">User<br><br>
+          <label for="" style="font-family: 'Open Sans', sans-serif;color: #399649;">Email:<br></label>
+          <input type="text" name="mail" class="email">
+          <br><br>
+          <label for="" style="font-family: 'Open Sans', sans-serif;color: #399649;">Password<br></label>
+          <input type="password" name="pass" class="pass">
+          <br><br>
+          <a href="forgotpass.php">Forget Your Password???<br><br></a>
+          <button type="submit" style="color:#399649;">Log In</button>
+          <br><br>
+                <a href="home.php">Go Home<br><br></a>
+
+          </fieldset>
+          <br><br>
+        </form>
+
+
+
+        <div class="footer">
+            <div class="wrap">
+            <div class="section group">
+                <div class="col_1_of_4 span_1_of_4">
+                    <h3>Credits</h3>
+                    <p>Feel free to contact us
+                        <a href="contact.html">here.</a>
+                    </p>
+                </div>
+                <div class="col_1_of_4 span_1_of_4">
+                    <h3>Latest News:</h3>
+                    <p>New Android App has been uploaded daily now.</p>
+                    <p>iOS updates are more advanced.</p>
+                    <p>LINUX apps getting more accepted by people.</p>
+                </div>
+                <div class="col_1_of_4 span_1_of_4">
+                    <h3>Store Location</h3>
+                    <p>Yet to be announced. :p</p>
+                    <h3>Order-online</h3>
+                    <p>01685-604351</p>
+                    <p>123-345-678</p>
+                </div>
+                <div class="col_1_of_4 span_1_of_4 footer-lastgrid">
+                    <h3>News-Letter</h3>
+                    <input type="text"><input type="submit" value="go" />
+                    <h3>Follow Us:</h3>
+                     <ul>
+                        <li><a href="https://twitter.com/?lang=en"><img src="images/twitter.png" title="twitter" />Twitter</a></li>
+                        <li><a href="https://www.facebook.com/"><img src="images/facebook.png" title="Facebook" />Facebook</a></li>
+                        <li><a href="https://www.rss.com/"><img src="images/rss.png" title="Rss" />Rss</a></li>
+                     </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="clear"> </div>
+        <div class="wrap">
+        <div class="copy-right">
+            <p>Mobilestore  &#169    All Rights Reserved | Design By <a href="contact.html">Roll08&12</a></p>
+        </div>
+        </div>
+        </div>
+        <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  </body>
+</html>
